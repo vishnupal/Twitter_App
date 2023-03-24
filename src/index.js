8f76fdb7b0c3ed7ce5aedd1ea37d88d2cd6e9342
vishnupal <vishnupalgehlot@gmail.com>
2023-03-24 17:32:49 +0530

    Added controllers and Routes

--- src/index.js
+++ src/index.js
@@ -1,17 +1,15 @@
 import express from 'express';
 import { connect } from './config/database.js';
-import service from './services/tweet-service.js';
-
+import apiRoutes from './routes/index.js';
 const app = express();
 
 const PORT = 3000;
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use('/api', apiRoutes);
 
 app.listen(PORT, async () => {
   console.log(`Server Start on port ${PORT}`);
   await connect();
   console.log('Mongo db connected');
-  const ser = new service();
-  await ser.create({
-    content: 'Hi #PaRTy kesi bolti public matchre bhidu log ',
-  });
 });
