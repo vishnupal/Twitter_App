e531444996628c0aa6ddaa60c0edaf9e7841dc19
vishnupal <vishnupalgehlot@gmail.com>
2023-03-26 16:06:42 +0530

    Added signup with encrypted password

---
+++ src/controllers/auth-controller.js
@@ -0,0 +1,25 @@
+import UserService from '../services/user-service.js';
+
+const userService = new UserService();
+export const signup = async (req, res) => {
+  try {
+    const response = await userService.signup({
+      email: req.body.email,
+      password: req.body.password,
+      name: req.body.name,
+    });
+    return res.status(201).json({
+      data: response,
+      err: {},
+      success: true,
+      message: 'User Successfully Signup',
+    });
+  } catch (error) {
+    return res.status(500).json({
+      data: {},
+      err: error,
+      success: false,
+      message: 'Failed to Signup',
+    });
+  }
+};
--- src/index.js
+++ src/index.js
@@ -1,8 +1,7 @@
 import express from 'express';
 import { connect } from './config/database.js';
 import apiRoutes from './routes/index.js';
-import { TweetRepository, UserRepository } from './repository/index.js';
-import LikeService from './services/like-service.js';
+
 const app = express();
 
 const PORT = 3000;
@@ -14,19 +13,4 @@ app.listen(PORT, async () => {
   console.log(`Server Start on port ${PORT}`);
   await connect();
   console.log('Mongo db connected');
-
-  // const userRepo = new UserRepository();
-  // const tweetRepo = new TweetRepository();
-  // const tweets = await tweetRepo.getAll(0, 10);
-  // console.log(tweets);
-  // const users = await userRepo.getAll();
-  // console.log(users);
-
-  // // const user = await userRepo.create({
-  // //   email: 'vishnupal@admin.com',
-  // //   password: '123456',
-  // //   name: 'vishnu',
-  // // });
-  // const likeService = new LikeService();
-  // likeService.toggleLike(tweets[0].id, 'Tweet', users[1].id);
 });
